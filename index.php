42253600dafa3f66f32dbe4a316ecb1737fe4540
Maurich-Gellaco <99902448+Maurich-Gellaco@users.noreply.github.com>
2022-03-19 00:30:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 	<li><a href = "index.php"> HOME</a></li>
 	<li><a href = "/LT2/index.html"> LEARNING TASK 2</a></li>
 	<li><a href = "/LT3/index.html"> LEARNING TASK 3</a></li>
+	<li><a href = "/CLE1T/index.html"> CLE Midterm</a></li>
 </ul>
 </body>
 </html>
