88a8cd85a201b2a2449d05072a19d171eae7eaee
Maurich-Gellaco <99902448+Maurich-Gellaco@users.noreply.github.com>
2022-03-22 09:01:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
 <h1> Hello, I am Ma.Yna Maurich Gellaco, this is my first web page. </h1>
 <ul>
-	<li><a href = "index.php"> HOME</a></li>
+	<li><a href = "/index.php"> HOME</a></li>
 	<li><a href = "/LT2/index.html"> LEARNING TASK 2</a></li>
 	<li><a href = "/LT3/index.html"> LEARNING TASK 3</a></li>
 	<li><a href = "/CLE1MT/index.html"> CLE Midterm</a></li>
