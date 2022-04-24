b963b8ef33f863053923a7738eef090d2d5b6c87
Maurich-Gellaco <99902448+Maurich-Gellaco@users.noreply.github.com>
2022-04-25 07:39:25 +0800

    Add files via upload

---
+++ LT5/javas.js
@@ -0,0 +1,21 @@
+function clickEvent(obj) {
+	obj.innerHTML = "I am already one year old :)";
+	
+
+}
+
+function hoverEvent(obj){
+	obj.innerHTML = "Hello!! My name is MURPHY!!";
+	
+	
+}
+
+function unHoverEvent(obj){
+	obj.innerHTML = "Hello!! My name is MURPHY!!";
+}
+
+function downEvent(obj){
+	obj.innerHTML = "Hotdogs, Carrots , Dog Food, Beef and Pork";
+
+
+	} 
