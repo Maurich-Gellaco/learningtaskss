fe15abc5bfa02a09ea5c3e62ac52c5c35268c505
Maurich-Gellaco <99902448+Maurich-Gellaco@users.noreply.github.com>
2022-05-20 16:19:17 +0800

    Add files via upload

---
+++ ENDTERM/nav.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+  <title>Navigation</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet">
+  <link rel="sylesheet" href="stylesheet.css">
+</head>
+<body>
+  <div class ="navbar">
+    <ul>
+      <li><button class="butn"><a href ="index.php">HOME</a></button></li>
+      <li><button class="butn"><a href ="works.php">WORKS</a></button></li>
+      <li><button class="butn"><a href ="aboutme.php">ABOUT ME</a></button></li>
+    </ul>
+  </div>
+</body>
+</html> 
+  
